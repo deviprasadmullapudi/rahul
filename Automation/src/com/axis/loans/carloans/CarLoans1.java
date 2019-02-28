0b6833ce16dca775f624bd5f6b3a6ad645bd5e36
deviprasadmullapudi <deviprasad.mullapudi@gmail.com>
2019-02-28 10:10:16 +0530

    Added Automation

---
+++ Automation/src/com/axis/loans/carloans/CarLoans1.java
@@ -0,0 +1,10 @@
+package com.axis.loans.carloans;
+
+public class CarLoans1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Automation/src/com/axis/loans/homeloans/HomeLoans1.java
@@ -0,0 +1,10 @@
+package com.axis.loans.homeloans;
+
+public class HomeLoans1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
