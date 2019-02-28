ddd46c26f8552836608faeeb61e8dc89c1367636
deviprasadmullapudi <deviprasad.mullapudi@gmail.com>
2019-02-28 19:00:05 +0530

    One statement added

--- Automation/src/com/axis/loans/carloans/CarLoans1.java
+++ Automation/src/com/axis/loans/carloans/CarLoans1.java
@@ -1,10 +1,11 @@
 package com.axis.loans.carloans;
 
-public class CarLoans1 {
+public class CarLoans1 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println(10);
 	}
 
 }
